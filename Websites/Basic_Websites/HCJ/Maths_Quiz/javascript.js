3b331862b3702bdb8270453e53e7da07250a5a40
Your Name <you@example.com>
2023-09-10 12:17:58 +0530

    Maths Quiz Website Final Update

--- Websites/Basic_Websites/HCJ/Maths_Quiz/javascript.js
+++ Websites/Basic_Websites/HCJ/Maths_Quiz/javascript.js
@@ -7,57 +7,57 @@ var correctAnswer;
 
 //if we click on the start/reset
 document.getElementById("startreset").onclick = function(){
-    
+
     //if we are playing
-    
+
     if(playing == true){
-        
+
         location.reload(); //reload page
-        
+
     }else{//if we are not playing
-        
+
         //change mode to playing
-        
+
         playing = true;
-        
+
         //set score to 0
-        
+
         score = 0;
         document.getElementById("scorevalue").innerHTML = score;
-     
-        //show countdown box 
-        
+
+        //show countdown box
+
         show("timeremaining");
         timeremaining = 60;
         document.getElementById("timeremainingvalue").innerHTML = timeremaining;
-        
+
         //hide game over box
-        
+
         hide("gameOver");
-        
+
         //change button to reset
         document.getElementById("startreset").innerHTML = "Reset Game";
-        
+
         //start countdown
-        
+
         startCountdown();
-        
+
         //generate a new Q&A
-        
+
         generateQA();
     }
-    
+
 }
 
 //Clicking on an answer box
 
 for(i=1; i<5; i++){
     document.getElementById("box"+i).onclick = function(){
-    //check if we are playing     
+    //check if we are playing
     if(playing == true){//yes
         if(this.innerHTML == correctAnswer){
         //correct answer
-            
+
             //increase score by 1
             score++;
             document.getElementById("scorevalue").innerHTML = score;
@@ -65,22 +65,22 @@ for(i=1; i<5; i++){
             hide("wrong");
             show("correct");
             setTimeout(function(){
-                hide("correct");   
+                hide("correct");
             }, 1000);
-            
+
             //Generate new Q&A
-            
+
             generateQA();
         }else{
         //wrong answer
             hide("correct");
             show("wrong");
             setTimeout(function(){
-                hide("wrong");   
+                hide("wrong");
             }, 1000);
         }
     }
-}   
+}
 }
 //if we click on answer box
     //if we are playing
@@ -104,32 +104,32 @@ function startCountdown(){
         if(timeremaining == 0){// game over
             stopCountdown();
             show("gameOver");
-         document.getElementById("gameOver").innerHTML = "<p>Game over!</p><p>Your score is " + score + ".</p>";   
+         document.getElementById("gameOver").innerHTML = "<p>Game over!</p><p>Your score is " + score + ".</p>";
             hide("timeremaining");
             hide("correct");
             hide("wrong");
             playing = false;
             document.getElementById("startreset").innerHTML = "Start Game";
         }
-    }, 1000);    
+    }, 1000);
 }
 
 //stop counter
 
 function stopCountdown(){
-    clearInterval(action);   
+    clearInterval(action);
 }
 
 //hide an element
 
 function hide(Id){
-    document.getElementById(Id).style.display = "none";   
+    document.getElementById(Id).style.display = "none";
 }
 
 //show an element
 
 function show(Id){
-    document.getElementById(Id).style.display = "block";   
+    document.getElementById(Id).style.display = "block";
 }
 
 //generate question and multiple answers
@@ -137,20 +137,55 @@ function show(Id){
 function generateQA(){
     var x = 1+ Math.round(9*Math.random());
     var y = 1+ Math.round(9*Math.random());
-    correctAnswer = x*y;
-    document.getElementById("question").innerHTML = x + "x" + y;
+    var z = 1+ Math.round(4*Math.random());
+    switch(z)
+    {
+        case 1:
+        correctAnswer = x+y;
+        document.getElementById("question").innerHTML = x + "+" + y;
+        break;
+        case 2:
+        correctAnswer = x-y;
+        document.getElementById("question").innerHTML = x + "-" + y;
+        break;
+        case 3:
+        correctAnswer = x*y;
+        document.getElementById("question").innerHTML = x + "x" + y;
+        break;
+        case 4:
+        correctAnswer = x/y;
+        correctAnswer=correctAnswer.toFixed(2);
+        document.getElementById("question").innerHTML = x + "/" + y;
+        break;
+    }
     var correctPosition = 1+ Math.round(3*Math.random());
     document.getElementById("box"+correctPosition).innerHTML = correctAnswer; //fill one box with the correct answer
-    
+
     //fill other boxes with wrong answers
-    
+
     var answers = [correctAnswer];
-    
+
     for(i=1; i<5; i++){
         if(i != correctPosition) {
-            var wrongAnswer;
+            var wrongAnswer;var m;
             do{
-                wrongAnswer = (1+ Math.round(9*Math.random()))*(1+ Math.round(9*Math.random())); //a wrong answer
+                 m = 1+ Math.round(4*Math.random());
+                switch(m)
+                {
+                     case 1:
+                    wrongAnswer = (1+ Math.round(9*Math.random()))+(1+ Math.round(9*Math.random()));
+                    break;
+                    case 2:
+                    wrongAnswer = (1+ Math.round(9*Math.random()))-(1+ Math.round(9*Math.random()));
+                     break;
+                     case 3:
+                    wrongAnswer = (1+ Math.round(9*Math.random()))*(1+ Math.round(9*Math.random()));
+                    break;
+                    case 4:
+                    wrongAnswer = (1+ Math.round(9*Math.random()))/(1+ Math.round(9*Math.random()));
+                    wrongAnswer=wrongAnswer.toFixed(2);
+                     break;
+                } //a wrong answer
             }while(answers.indexOf(wrongAnswer)>-1)
             document.getElementById("box"+i).innerHTML = wrongAnswer;
             answers.push(wrongAnswer);

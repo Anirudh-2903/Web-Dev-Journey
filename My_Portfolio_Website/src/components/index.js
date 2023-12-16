60194f78efbed51c1e858b438b5efb8fcacc07ca
Your Name <you@example.com>
2023-12-16 08:53:40 +0530

    repaired

--- My_Portfolio_Website/src/components/index.js
+++ My_Portfolio_Website/src/components/index.js
@@ -1,15 +1,27 @@
-import CTA from "./CTA";
-import Alert from "./Alert";
-import Loader from "./Loader";
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from "./Hero";
 import Navbar from "./Navbar";
-import Footer from "./Footer";
-import HomeInfo from "./HomeInfo";
+import About from "./About";
+import Tech from "./Tech";
+import Experience from "./Experience";
+import Works from "./Works";
+import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
+
 
 export {
-    CTA,
-    Alert,
-    Loader,
-    Navbar,
-    Footer,
-    HomeInfo
-}
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+};
